387bf35c845036113f175e5ddf238b3cec38f6f9
ho <>
2023-03-24 17:01:48 +0900

    add!!!!

---
+++ build-run-dev.sh
@@ -0,0 +1,2 @@
+yarn build run
+scp -r -p build/* testreact@18.234.105.159:/home/testreact/test-react-v2
---
+++ pull-run-build.sh
@@ -0,0 +1,3 @@
+scp -r -p 22 git@github.com:yoodalho/react-test.git /Users/yoodalho/react-test
+#yarn build run
+
